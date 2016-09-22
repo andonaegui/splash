d3a03a30e4de485d7f0cb7adb6acf7aa71690692
Andonaegui <andonaegui@gmail.com>
2016-09-22 16:09:01 -0300

    Initial commit

---
+++ app/src/main/java/com/example/randona/welcomeanimationapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.randona.welcomeanimationapp;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to exit?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //MainActivity.super.onBackPressed();
+                        moveTaskToBack(true);
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+
+}
